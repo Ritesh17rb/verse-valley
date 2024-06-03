4cc5c49b2fad6d94746949d30376d208a76c2287
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-06-04 00:06:21 +0530

    All files

---
+++ Frontend/src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <iframe
+        title="External Website"
+        src="https://portfolio-five-phi-46.vercel.app/"
+        width="100%"
+        height="600px"
+        frameBorder="0"
+        scrolling="auto"
+      />
+    </div>
+  );
+};
+
+export default About;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,83 @@
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import PostCard from '../components/PostCard';
+import Typewriter from '../components/Typewriter'; // Import Typewriter component
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  const imageUrls = [
+    'https://source.unsplash.com/random/300×300?poems',
+    'https://source.unsplash.com/random/300×300?coding',
+    'https://source.unsplash.com/random/300×300?poems',
+    "https://source.unsplash.com/random/300×300?book",
+    "https://source.unsplash.com/random/300×300?poems",
+    "https://source.unsplash.com/random/300×300?technology",
+  ];
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+      <div className="flex items-center">
+        <h1 className='text-3xl font-bold lg:text-6xl'>
+          <Typewriter text="VerseValley" delay={100} />
+        </h1>
+      </div>
+
+      <h1 className='font-bold text-pink-400 text-lg lg:text-lg'>
+        <Typewriter text="Where words bloom and stories thrive. Welcome to a vibrant community of writers and storytellers. Dive into our world of inspiration and creativity." delay={50} />
+      </h1>
+      <Link
+        to='/search'
+        className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
+      >
+        View all posts
+      </Link>
+      <div className='carousel-container'>
+        <Carousel
+          showThumbs={false}
+          autoPlay
+          infiniteLoop
+          interval={3000}
+          transitionTime={500}
+          showStatus={false}
+          showIndicators={false}
+        >
+          {imageUrls.map((url, index) => (
+            <div key={index} className='carousel-image-wrapper'>
+              <img src={url} alt={`Carousel image ${index + 1}`} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {posts && posts.length > 0 && (
+          <div className='flex flex-col gap-6'>
+            <h2 className='text-2xl font-semibold text-center'>Posts</h2>
+            <div className='flex flex-wrap gap-12'>
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={'/search'}
+              className='text-lg text-teal-500 hover:underline text-center'
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
