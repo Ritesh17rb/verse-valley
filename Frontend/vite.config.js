3ccbeb439d47646353c402f28d5e06cc46609b91
Ritesh17rb <ritesh17lifeamazing@gmail.com>
2024-06-04 00:29:50 +0530

    corrected logs

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-// vite.config.js
-
-// export default {
-//   server: {
-//     host: 'localhost',
-//   },
-// };
-
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+        target: 'https://verse-valley.vercel.app/',
+        secure: true, // Set to true if your backend server uses HTTPS
+        changeOrigin: true, // Required for Vercel backend
       },
     },
   },
